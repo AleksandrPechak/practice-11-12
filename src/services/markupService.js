a8d4c7d7cab44e5c70354275f38eaecb2057cbdd
MSvidlo <143841475+MSvidlo@users.noreply.github.com>
2024-02-18 15:14:34 +0100

    task2

--- src/services/markupService.js
+++ src/services/markupService.js
@@ -11,3 +11,14 @@ export function markupAllProducts(products) {
     </li>`;
   }).join(""); 
 }
+
+export const markupProductId = ({ images, brand, category, description, id, title, price }) =>
+{
+  return `  <li data-id="${id}">
+      <img src="${images[0]}" alt="${title}" />
+      <h2>Brand:${brand}</h2>
+      <p>Category:${category}</p>
+      <p>Model:${title}</p>
+      <p>Desc:${description}</p>
+      <p>Price:${price}</p>
+    </li>`}
